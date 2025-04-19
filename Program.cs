89c5a3c01150c598c0155ad69bd363cee218073c
ST10318880_Kenan <163179117+KenanVC@users.noreply.github.com>
2025-04-19 10:28:14 +0200

    Dotnet project initialised + functional wav greeting

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Media;
+
+namespace ST10318880_POE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer("greeting.wav");
+            player.PlaySync();
+        }
+    }
+}
