343c8cd3e1f3dc91bdff2a7f443eae375230ddc3
ST10318880_Kenan <163179117+KenanVC@users.noreply.github.com>
2025-06-25 12:56:23 +0200

    Restructure repo to root-level and include all projects

---
+++ ChatbotGUI/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ChatbotGUI
+{
+    public partial class App : Application
+    {
+        public static string? UserName { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (e.Args.Length > 0)
+            {
+                UserName = e.Args[0];
+            }
+        }
+    }
+}
